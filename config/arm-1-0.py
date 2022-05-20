282e00725779277551fbf60fc70698f1d5ffaef0
Patrick Carroll <patrickgesarcarroll@gmail.com>
2022-05-19 22:36:53 -0700

    arm-1-1 configuration

--- config/arm-1-0.py
+++ config/arm-1-0.py
@@ -1,6 +1,6 @@
-min = [0, 0, 0, 0, 0, 0]
+min = [0, 0, 90, 0, 0, 90]
 max = [180, 150, 180, 180, 180, 155]
-reverse = [False, True, True, False, False, False]
+reverse = [False, False, False, True, True, False]
 resting = [105,20,175,0,110,150]
 
 def lerp(index, value):
