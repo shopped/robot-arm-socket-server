6dc7da66bf6dc8282b2d167580b0b8cb51d1ef91
Patrick Carroll <patrickgesarcarroll@gmail.com>
2022-05-20 00:34:18 -0700

    Update arm-0-0 wrist control

--- config/arm-0-0.py
+++ config/arm-0-0.py
@@ -4,6 +4,11 @@ reverse = [False, True, False, False, True, False]
 resting = [95,15,180,0,50,30]
 
 def lerp(index, value):
+    if (index == 4):
+        if (value < 90):
+            return int(170 - (value)*(125/90))
+        else:
+            return 45
     new_value = 180 - value if reverse[index] else value
     new_value = min[index] + (new_value/180) * (max[index] - min[index])
     return new_value
