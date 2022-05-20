a671cb9c1d0351430f7b24666b86de17189d9fde
Patrick Carroll <patrickgesarcarroll@gmail.com>
2022-05-20 00:05:57 -0700

    Update arm 0-1 config

--- config/arm-0-1.py
+++ config/arm-0-1.py
@@ -1,9 +1,14 @@
-min = [0, 0, 0, 0, 0, 10]
+min = [0, 0, 90, 0, 0, 10]
 max = [180, 180, 180, 180, 180, 100]
-reverse = [False, True, False, False, True, False]
+reverse = [False, False, False, True, True, False]
 resting = [90,0,160,0,170,30]
 
 def lerp(index, value):
+    if (index == 4):
+        if (value > 90):
+            return int(180 - (value - 90)*(110/90))
+        else:
+            return 180
     new_value = 180 - value if reverse[index] else value
     new_value = min[index] + (new_value/180) * (max[index] - min[index])
     return new_value
