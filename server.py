844c49d53b71b02792dc4684e5426c6cf6ab9e7a
Patrick Carroll <patrickgesarcarroll@gmail.com>
2022-05-22 13:22:48 -0700

    Part 3

--- server.py
+++ server.py
@@ -59,14 +59,14 @@ def set_recording():
 
 def set_playback(quarter): #(0/1/2/3)
     for index, address in enumerate(action_pixels):
-        if (quarter * 2 == index) or (quarter * 2 - 1 == index):
+        if (quarter2 == index) or (quarter * 2 + 1 == index):
             pixels[address] = BLUE
         else:
             pixels[address] = CLEAR
 
 def set_looping(quarter):
     for index, address in enumerate(action_pixels):
-        if (quarter * 2 == index) or (quarter * 2 - 1 == index):
+        if (quarter * 2 == index) or (quarter * 2 + 1 == index):
             pixels[address] = BLUE
         else:
             pixels[address] = WHITE
